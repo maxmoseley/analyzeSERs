4b1066e240915726a7b7527edbadf93bdebe138c
Maxwell.Moseley <Maxwell.Moseley@JLHA-LT-332.jlha.local>
2018-09-06 11:54:07 -0400

    Initial commit

---
+++ radarAnalyze.py
@@ -0,0 +1,103 @@
+'''
+Created on Aug 28, 2018
+
+@author: Maxwell.Moseley
+'''
+
+from itertools import combinations
+import pandas as pd
+import statsmodels.api as sm
+import numpy as np
+from time import strftime
+import os
+
+if __name__ == '__main__':
+    
+    RadarFile = "Data/2018-08-30 Radar SERs Data.xlsx"
+    ResultsFile = "Results/%s-%s-%s Radar SERs Analysis %s%s.xlsx" % (strftime("%Y"),strftime("%m"),strftime("%d"),strftime("%H"),strftime("%M"))
+    writer = pd.ExcelWriter(ResultsFile, engine='openpyxl')
+    ResultsDF = pd.DataFrame()
+    
+    numVars = 3
+    numPoints = 12
+    
+    RadarDurs = pd.read_excel(RadarFile,sheet_name="Durations",index_col=0)
+    RadarParams = pd.read_excel(RadarFile,sheet_name="Technical Parameters",index_col=0)
+    RadarQuals = pd.read_excel(RadarFile,sheet_name="Other",index_col=0)
+    
+    for col in RadarDurs.columns:
+        RadarDurs["ln_%s"%col] = np.log(RadarDurs[col]).replace([np.inf, -np.inf],np.nan)
+    
+    for col in RadarParams.columns:
+        RadarParams["ln_%s"%col] = np.log(RadarParams[col]).replace([np.inf, -np.inf],np.nan)
+        RadarParams["%s^2"%col] = (RadarParams[col]**2).replace([np.inf, -np.inf],np.nan)
+        RadarParams["sqrt(%s)"%col] = np.sqrt(RadarParams[col]).replace([np.inf, -np.inf],np.nan)
+        RadarParams["e^(%s)"%col] = np.exp(RadarParams[col]).replace([np.inf, -np.inf],np.nan)
+
+skipCases = 0
+testCases = 0
+
+while numVars >= 1:
+    for i in RadarDurs.columns:
+        for param in list(combinations(RadarParams.columns,numVars)):
+            param = np.asarray(param)
+            testSet = pd.concat([RadarDurs[i],RadarParams[param]], axis=1)
+            testSet = testSet.replace([np.inf, -np.inf],np.nan)
+            testSet = testSet.dropna(axis='rows',how='any')
+            if not testSet.empty:
+                if len(testSet.index) >= numPoints:
+                    y = RadarDurs[i]
+                    x = RadarParams[param]
+                    x = sm.add_constant(x)
+                    model = sm.OLS(y,x,missing='drop')
+                    results = model.fit()
+                    paramCoeff = { str(key):value for key,value in results.params.to_dict().items() }
+                    paramPvals = { str(key):value for key,value in results.pvalues.to_dict().items() }
+                    paramTvals = { str(key):value for key,value in results.tvalues.to_dict().items() }
+                    paramStdErr = { str(key):value for key,value in results.bse.to_dict().items() }
+                    yDur = model.data.ynames
+                    xParams = [str(m) for m in model.data.xnames]
+                    
+                    ResultVals = {
+                        "Number of X Variables": numVars,
+                        "Count of Usable Radars": results.nobs,
+                        "Duration": yDur,
+                        "Parameters": xParams,
+                        "R-Squared": results.rsquared,
+                        "Adj R-Squared": results.rsquared_adj,
+                        "F Test": results.fvalue,
+                        "Prob(F Test)": results.f_pvalue,
+                        "Degrees of Freedom": results.df_model,
+                        "Parameter Coefficients": paramCoeff,
+                        "Parameter p-values": paramPvals,
+                        "Parameter t-values": paramTvals,
+                        "Parameter Std Err": paramStdErr,
+                        "Model MSE": results.mse_model,
+                        "Residual MSE": results.mse_resid,
+                        "Total MSE": results.mse_total,
+                        "Model Type": results.model.__class__.__name__
+                        }
+                    
+                    ResultsDF = ResultsDF.append(ResultVals, ignore_index=True)
+                    
+                    if not os.path.isfile(ResultsFile):
+                        ResultsDF.tail(1).to_excel(writer, header=ResultsDF.columns, index=False)
+                    else:
+                        ResultsDF.tail(1).to_excel(writer, startrow=len(ResultsDF), header=False, index=False)
+                    
+                    writer.save()
+                    
+                    testCases = testCases + 1
+                
+                else:
+                    skipCases = skipCases + 1
+            
+            else:
+                skipCases = skipCases + 1
+            
+            print "Tested: %s, Skipped: %s" % (testCases,skipCases)
+    
+    numVars = numVars - 1
+       
+print "Done! :)"   
+pass
